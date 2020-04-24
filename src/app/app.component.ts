fbb1ca82d24d566ad63628143d73c102ffc391ac
Michał <barczykmt@gmail.com>
2020-04-24 14:02:29 +0200

    [POWRNG-21] review fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Questionnaire } from './questionnaire';
+import { Question } from './question';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -10,12 +12,12 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit{
   
   title = 'uxTestFrontend';
-
-  readonly URL = 'http://localhost:8099/api/questionnaire';
+  host = 'http://localhost:8099';
+  questionnairesEndpoint = '/api/questionnaire';
 
   public questionnaireName: string = ''
 
-  public questions: any[] = [{
+  public rawQuestions: any[] = [{
     id: '',
     content: '' 
   }];
@@ -31,37 +33,38 @@ export class AppComponent implements OnInit{
   }
 
   addQuestion() {
-    this.questions.push({
-      id: this.questions.length + 1,
+    this.rawQuestions.push({
+      id: this.rawQuestions.length + 1,
       content: ''
     });
   }
 
   removeRecentlyAddedQuestion() {
-    this.questions.pop(); 
+    this.rawQuestions.pop(); 
   }
 
   submitQuestionnaire() {
-    
-    var body = {
+
+    var body : Questionnaire = {
       name: this.questionnaireName,
-      questions: this.questions
+      questions: this.rawQuestions.map(question => question.content)
     }
 
-    this.http.post(this.URL, body)
+    var url = this.host + this.questionnairesEndpoint
+
+    this.http.post(url, body)
       .toPromise()
       .then(data => {
         console.log(data)
       })
-
-    
   }
 
   logValue() {
-    console.log(this.questions);
+    console.log(this.rawQuestions);
   }
 
   getAllQuestionnaires() {
-    this.questionnaires = this.http.get(this.URL)
+    var url = this.host + this.questionnairesEndpoint
+    this.questionnaires = this.http.get(url)
   }
 }
