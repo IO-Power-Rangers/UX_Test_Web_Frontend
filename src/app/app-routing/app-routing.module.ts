1d9b4ec83de5fcae606e44cccc83c3c297d5c507
Michał <barczykmt@gmail.com>
2020-04-27 20:57:43 +0200

    [POWRNG-31] integrate questionnaire component with router

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,6 +4,7 @@ import {HomePageComponent} from '../components/home-page/home-page.component';
 import {CreateTestsComponent} from '../components/create-tests/create-tests.component';
 import {SignInOutComponent} from '../components/sign-in-out/sign-in-out.component';
 import {ViewTestsComponent} from '../components/view-tests/view-tests.component';
+import {CreateQuestionnaireComponent} from '../components/create-questionnaire/create-questionnaire.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'signInUp',
     component: SignInOutComponent,
+  },
+  {
+    path: 'createQuestionnaire',
+    component: CreateQuestionnaireComponent,
   },
 ];
 
