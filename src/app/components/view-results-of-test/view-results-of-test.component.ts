d442b72ae66a2dd6d4228df2b59725c38cf24cb1
misikora <mikolajsi@student.agh.edu.pl>
2020-05-16 10:08:35 +0200

    Add html

--- src/app/components/view-results-of-test/view-results-of-test.component.ts
+++ src/app/components/view-results-of-test/view-results-of-test.component.ts
@@ -20,21 +20,52 @@ export class ViewResultsOfTestComponent implements OnInit {
   host = 'http://localhost:8099';
   testsEndpoint = '/api/cardsorting/tests/';
   testID : bigint;
-  results : Object;
+  processedResults = [];
 
   getResults(){
     this.http.get(this.host + this.testsEndpoint + this.testID + "/results")
       .toPromise()
       .then(data => {
-        this.results = data;
-        var para = document.createElement("p");
-        var node = document.createTextNode(JSON.stringify(data));
-        para.appendChild(node);
-
-        var element = document.getElementById("import");
-        element.appendChild(para);
+        this.processResults(data);
         console.log(data);
       });
+  }
+
+  processResults(results){
+    if(results.length === 0) return;
+    let categoriesNames = [];
+    let subjectNames = [];
+    const categories = results[0].test.categories;
+    for(let i = 0; i < categories.length; i++){
+      categoriesNames.push(categories[i].name);
+    }
+    const subjects = results[0].test.subjects;
+    for(let i = 0; i < subjects.length; i++){
+      subjectNames.push(subjects[i].name);
+    }
+    for(let categoryName of categoriesNames){
+      let occurrenceCounter = {};
+      for(let subjectName of subjectNames){
+        occurrenceCounter[subjectName] = 0;
+      }
+      for(let result of results){
+          for(let categoryWithSubject of result.categoriesWithSubjects){
+              if(categoryWithSubject.category.name === categoryName){
+                for(let subject of categoryWithSubject.subjects){
+                  occurrenceCounter[subject['name']]++;
+                }
+              }
+          }
+      }
+      let sortedOccurrenceCounter = Object.keys(occurrenceCounter).map(function(key) {
+        return [key, occurrenceCounter[key]];
+      });
+
+      sortedOccurrenceCounter.sort(function(first, second) {
+        return second[1] - first[1];
+      });
+      this.processedResults.push([categoryName, sortedOccurrenceCounter]);
+    }
   }
 
 }
