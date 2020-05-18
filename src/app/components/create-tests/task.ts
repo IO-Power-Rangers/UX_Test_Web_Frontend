8cde332f1c388fe6c9c6fe7cf3dc1bcf27e2d3b6
Hewwho <50575563+Hewwho@users.noreply.github.com>
2020-05-19 01:31:27 +0200

    Hiding stuff for now

---
+++ src/app/components/create-tests/task.ts
@@ -0,0 +1,5 @@
+export interface Task {
+  index: bigint;
+  name: string;
+  description: string;
+}
---
+++ src/app/components/create-tests/test.ts
@@ -0,0 +1,8 @@
+import { Task } from './task';
+import { UxModel } from './uxModel';
+
+export interface Test {
+  uxModel: UxModel;
+  title: string;
+  tasks: Task[];
+}
