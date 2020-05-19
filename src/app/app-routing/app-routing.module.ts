9076950a9d2c1b9bb850f3f48334d5a13a2a6fe6
Hewwho <maszlasz3@gmail.com>
2020-05-19 07:57:43 +0200

    Temporarily brought back a component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,7 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from '../components/home-page/home-page.component';
 import {CreateTestsComponent} from '../components/create-tests/create-tests.component';
 import {SignInOutComponent} from '../components/sign-in-out/sign-in-out.component';
-// import {ViewTestsComponent} from '../components/view-tests/view-tests.component';
+import {ViewTestsComponent} from '../components/view-tests/view-tests.component';
 import {CreateQuestionnaireComponent} from '../components/create-questionnaire/create-questionnaire.component';
 import {ScreenRecordingComponent} from '../components/screen-recording/screen-recording.component';
 import {RecordingPermissionViewComponent} from '../components/recording-permission-view/recording-permission-view.component';
@@ -35,10 +35,10 @@ const routes: Routes = [
     component: CreateTestsComponent,
     canDeactivate: [PendingChangesGuard],
   },
-  // {
-  //   path: 'viewTests',
-  //   component: ViewTestsComponent,
-  // },
+  {
+    path: 'viewTests',
+    component: ViewTestsComponent,
+  },
   {
     path: 'signInUp',
     component: SignInOutComponent,
