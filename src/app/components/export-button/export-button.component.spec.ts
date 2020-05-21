09733f18f8f3ca503487ce4f94db5ba24e9791f7
Rafał Lisak <rafal.lisak1@gmail.com>
2020-05-21 15:45:35 +0200

    CI import added

--- src/app/components/export-button/export-button.component.spec.ts
+++ src/app/components/export-button/export-button.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExportButtonComponent } from './export-button.component';
+import { CommonTestModule } from 'src/app/common-test/common-test.module';
 
 describe('ExportButtonComponent', () => {
   let component: ExportButtonComponent;
@@ -8,6 +9,7 @@ describe('ExportButtonComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [CommonTestModule],
       declarations: [ ExportButtonComponent ]
     })
     .compileComponents();
