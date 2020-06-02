c45609951963f0ac769bb4886c67e1287df0a097
Filip Zybała <fzybala@virtuslab.com>
2020-06-02 16:31:37 +0200

    Deployment configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl : "https://ux-test-power-rangers.herokuapp.com",
-  local : "http://localhost:8099",
-  users : "/api/users",
+  apiUrl : 'https://ux-test-power-rangers.herokuapp.com',
+  local : 'http://localhost:8099',
+  users : '/api/users',
+  tests : '/api/tests',
+  results: '/api/results',
+  export: '/api/export'
 };
