992708eaa5e4943392bb7654df129cc79153c60f
Piotr Pasternak <piotrpassternak@gmail.com>
2020-06-13 21:58:07 +0200

    Add ne group button

--- src/app/components/view-tests/view-tests.component.ts
+++ src/app/components/view-tests/view-tests.component.ts
@@ -3,7 +3,7 @@ import {Title} from '@angular/platform-browser';
 import {ViewTestsService} from '../../services/view-tests.service';
 import {Test} from '../../../interfaces/test';
 import {Router} from '@angular/router';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, CdkDropList, DragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import {UserService} from '../../services/user.service';
 import {ExportButtonComponent} from '../export-button/export-button.component';
 import {TestGroup} from '../../../interfaces/testGroup';
@@ -24,12 +24,13 @@ export class ViewTestsComponent implements OnInit {
   message: Test;
   groupedTests: GroupedTest[];
   newTests: GroupedTest[];
-  ungroupedTests: GroupedTest[];
+  counter: number;
 
   constructor(private titleService: Title, private viewTestsService: ViewTestsService, private router: Router,
               private userService: UserService, public exportButton: ExportButtonComponent) {
     this.titleService.setTitle('View your tests');
     this.groups = [];
+    this.counter = 0;
     this.showTest();
   }
 
@@ -72,30 +73,23 @@ export class ViewTestsComponent implements OnInit {
   groupTests() {
     this.groupedTests = [];
     this.newTests = [];
-    this.ungroupedTests = [];
     let added = false;
-    if (this.groups.length !== 0) {
-      for (const test1 of this.tests) {
-        for (const group of this.groups) {
-          if (group.id === test1.id) {
-            const newGropedTest = {test: test1, name: group.name};
-            this.groupedTests.push(newGropedTest);
-            added = true;
-          }
+    for (const test1 of this.tests) {
+      for (const group of this.groups) {
+        if (group.id === test1.id) {
+          const newGropedTest = {test: test1, name: group.name};
+          this.groupedTests.push(newGropedTest);
+          added = true;
         }
-        if (added === false) {
-          const newGropedTest = {test: test1, name: ''};
-          this.newTests.push(newGropedTest);
-        }
-        added = false;
       }
-    } else {
-      for (const test1 of this.tests) {
+      if (added === false) {
         const newGropedTest = {test: test1, name: ''};
-        this.ungroupedTests.push(newGropedTest);
+        this.newTests.push(newGropedTest);
       }
+      added = false;
     }
   }
+
 
   drop(event: any) {
     if (event.previousContainer === event.container) {
@@ -106,6 +100,12 @@ export class ViewTestsComponent implements OnInit {
         event.previousIndex,
         event.currentIndex);
     }
+  }
+
+  addGroup() {
+    const name1 = 'new group ' + this.counter;
+    this.names.push(name1);
+    this.counter += 1;
   }
 
 
